64967738e1dc785eeae8fd109ebfa192df92609a
Atacan <atacan.g17@gmail.com>
2017-11-27 20:21:07 +0300

    görsel tasarımları yapıldı

--- app/src/main/java/com/kutuphanelerim/uzman/whereami/Main3Activity.java
+++ app/src/main/java/com/kutuphanelerim/uzman/whereami/Main3Activity.java
@@ -1,15 +1,13 @@
-package com.example.atc.okulprojesi;
+package com.kutuphanelerim.uzman.whereami;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.kutuphanelerim.uzman.whereami.R;
-
-public class MainActivity extends AppCompatActivity {
+public class Main3Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main3);
     }
 }
